998d17e2cbe24692b938a08bf21e535b32088708
Tlangelani Mboweni <mbowenit@gmail.com>
2020-05-21 23:55:48 +0200

    get splash images

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,10 +1,21 @@
 import React, { Component } from 'react';
 import SearchBar from './SearchBar';
+import axios from 'axios';
 
 class App extends Component {
     
-    onInputChange(term) {
-        console.log({term});
+   async  onInputChange(term) {
+        let url = 'https://api.unsplash.com';
+        const response = await axios.get(`${url}/search/photos`, {
+            params: {
+                query: term
+            },
+            headers: {
+                Authorization: 'Client-ID jHXjQT2YLBb-MtYRNT7Nhh6qxhjLycYYNd58JBzPLPA'
+            }
+        });
+
+        console.log(response.data.results);
     }
 
     render() {
