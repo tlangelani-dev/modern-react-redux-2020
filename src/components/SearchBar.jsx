29937d68d00e78b66c1f12b13c7a5993926932ba
Tlangelani Mboweni <mbowenit@gmail.com>
2020-05-21 21:35:38 +0200

    update searchbar

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -1,14 +1,23 @@
-import React from 'react';
+import React, { Component } from 'react';
 
-const SearchBar = () => {
-    return (
-        <div className="ui segment">
-            <form className="ui form">
-                <label htmlFor="input-search">Image Search</label>
-                <input type="text" id="input-search" />
-            </form>
-        </div>
-    );
-};
+class SearchBar extends Component {
+
+    onInputChange(e) {
+        console.log(e.target);
+    }
+
+    render() {
+        return (
+            <div className="ui segment">
+                <form className="ui form">
+                    <div className="field">
+                        <label htmlFor="input-search">Image Search</label>
+                        <input type="text" id="input-search" onChange={this.onInputChange} />
+                    </div>
+                </form>
+            </div>
+        );
+    }
+}
 
 export default SearchBar;
