896489559a733d380bb1c05ade4fda6eda55d36d
Tlangelani Mboweni <mbowenit@gmail.com>
2020-05-22 00:14:20 +0200

    refactor axios

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,27 +1,25 @@
 import React, { Component } from 'react';
 import SearchBar from './SearchBar';
-import axios from 'axios';
+import unsplash from '../api/unsplash';
 
 class App extends Component {
+
+    state = { images: []  };
     
-   async  onInputChange(term) {
-        let url = 'https://api.unsplash.com';
-        const response = await axios.get(`${url}/search/photos`, {
+    onInputChange = async (term) => {
+        const response = await unsplash.get('/search/photos', {
             params: {
                 query: term
-            },
-            headers: {
-                Authorization: 'Client-ID jHXjQT2YLBb-MtYRNT7Nhh6qxhjLycYYNd58JBzPLPA'
             }
         });
-
-        console.log(response.data.results);
+        this.setState({ images: response.data.results });
     }
 
     render() {
         return (
             <div className="ui container app">
                 <SearchBar onInputChange={this.onInputChange} />
+                <p>Found: { this.state.images.length } images</p>
             </div>
         );
     }
